bb4f1b2d6c86a2d8e9bd559d5d63a7f204573714
tashtag-code <tashgibson16@gmail.com>
2021-05-20 16:23:21 +1000

    added a welcome message and some questions to plant_shop

--- plant_shop.rb
+++ plant_shop.rb
@@ -1,4 +1,7 @@
 puts "Welcome to the indoor plant shop"
+puts "What is your name?"
+name = gets.chomp()
+puts ("Hello " + name + ", please answer some questions to find your perfect plant match.")
 
 
 questions = ["Question 1", "Question 2", "Question 3"]
@@ -8,6 +11,6 @@ questions.each do |question|
  answers << gets.strip
 end
 
-Question 1 = "Do you want a ({1}small), ({2}medium), ({3}large) plant?"
-Question 2 = "Do you want a ({1}easy) or ({2}challenging) plant?"
-Question 3 = "Do you want a low light or high light plant?"
+question 1 "Do you want a ({1}small), ({2}medium), ({3}large) plant?"
+question 2 "Do you want a ({1}easy) or ({2}challenging) plant?"
+question 3 "Do you want a low light or high light plant?"
