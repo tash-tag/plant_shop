d1458aa6a90a1a768cfa419f10e9285aba2147e7
tashtag-code <tashgibson16@gmail.com>
2021-05-26 15:01:10 +1000

    deleted unwanted code from main file

--- plant_shop.rb
+++ plant_shop.rb
@@ -8,19 +8,7 @@ require 'rspec'
 require 'tty-prompt'
 require 'artii'
 
-# if ARGV.length > 0
-#  flag, *rest = ARGV #flag first, then splat operator will create array, then the rest.
-#  ARGV.clear
-#  case flag
-#  when '-help'
-#   puts "See further documentation in readme"
-#   exit
-#  when '-info'
-#   puts "This program is using Ruby version: #{RUBY_VERSION}"
-#  else 
-#   puts "Invalid argument, see readme for options"
-#  end
-# end
+
 
 #When the program starts there will be a welcome message and it will ask for users name.
 def welcome
